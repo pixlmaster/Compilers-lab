817ece338ec7fd3e7b3b5d7cefb86c5211a555dc
unknown <akash.tiwari21aug@gmail.com>
2019-11-23 10:19:31 +0530

    add test 3

---
+++ assign5_17CS10003_17CS10035/assign5_17CS10035_17CS10003_test3.c
@@ -0,0 +1,30 @@
+// Test Statements
+int test = 3;
+int add (int a, int b) {
+	a = 10;
+	int *x, y;
+
+	// Pointer Assignments
+	
+	x = &y;
+	*x = y;
+	y = *x;
+}
+void main () {
+	int i, a[10], v = 3;
+	double d, d1;
+	for (i=1; i<a[10]; i++) 
+		i++;
+	do i = i - 1; while (a[i] < v);
+	i = 2;
+	if (i&&v) i = 1;
+	while (v>a[i]) {
+		i--;
+	}
+	
+	// Indexed Copy Instruction
+	a[4] = i;
+	v = a[i];
+
+	return;
+}
