8b9266923f41fd2dc39fdceb1347e2bab72a6d93
unknown <akash.tiwari21aug@gmail.com>
2019-11-23 10:19:24 +0530

    add test 2

---
+++ assign5_17CS10003_17CS10035/assign5_17CS10035_17CS10003_test2.c
@@ -0,0 +1,14 @@
+// Test Declaration
+int tst = 2;
+double d = 223.33;
+int i, w[10];
+int a = 4, *p, b;
+void func(int i, double d);
+char c;
+
+int main () {
+	double e = 21.3;
+	int f, w[3];
+	char c1;
+	int r1w = 42, *q, y;
+}
